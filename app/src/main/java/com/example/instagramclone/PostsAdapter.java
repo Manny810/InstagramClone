a8f53f75fd92e49bd17c942777069658afc401e8
Manny810 <mannyfavela2000@gmail.com>
2020-07-10 16:08:59 -0500

    implemented post detail activity

--- app/src/main/java/com/example/instagramclone/PostsAdapter.java
+++ app/src/main/java/com/example/instagramclone/PostsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.instagramclone;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +15,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
 
+import org.parceler.Parcels;
+
 import java.util.List;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
+    public static final String TAG = "PostsAdapter";
 
     private Context context;
     private List<Post> posts;
@@ -51,7 +56,8 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
     }
 
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         private TextView tvUsername;
         private ImageView ivImage;
@@ -63,6 +69,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
             tvUsername = itemView.findViewById(R.id.tvUsername);
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvDescription);
+            itemView.setOnClickListener(this);
         }
 
         public void bind(Post post) {
@@ -74,6 +81,29 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
                 Glide.with(context).load(image.getUrl()).into(ivImage);
             }
 
+        }
+
+        // when the user clicks on a row, show MovieDetailsActivity for the selected movie
+        @Override
+        public void onClick(View view) {
+            // gets item position
+            int position = getAdapterPosition();
+
+            // make sure the position is valid, i.e. actually exists in teh view
+            if (position != RecyclerView.NO_POSITION){
+                Log.d(TAG, "onClick with valid position called");
+                // get the movie at the position
+                Post post = posts.get(position);
+
+                // create intent for the new activity
+                Intent detail = new Intent(context, PostDetailActivity.class);
+
+                // serialize the movie using parceler, use its short name as a key
+                detail.putExtra(Post.class.getSimpleName(), Parcels.wrap(post));
+
+                // show the activity
+                context.startActivity(detail);
+            }
         }
     }
 }
